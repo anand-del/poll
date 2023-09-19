d5458e3d39dc9f2e384a28cd993b4fbc1eeb4931
unknown <ankit.sikshay27@gmail.com>
2023-09-19 21:21:07 +0530

    email code done

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,6 +41,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $user->sendEmailVerificationNotification();
+
 
         event(new Registered($user));
 
